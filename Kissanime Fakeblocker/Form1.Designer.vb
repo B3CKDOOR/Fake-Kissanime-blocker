ffc564bcb84f489b0779b3e11593d660d2abb72d
B3CKDOOR <rido1996@gmail.com>
2016-12-20 10:34:05 +0100

    Add project files.

---
+++ Kissanime Fakeblocker/Form1.Designer.vb
@@ -0,0 +1,115 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.Kissanime_Fakeblocker.My.Resources.Resources.Kissanime
+        Me.PictureBox1.Location = New System.Drawing.Point(12, 12)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(256, 256)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'Button1
+        '
+        Me.Button1.Enabled = False
+        Me.Button1.Location = New System.Drawing.Point(12, 347)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Undo Fix"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(196, 347)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "Exit"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Items.AddRange(New Object() {"Block all fake websites", "Block harmfull kissanime fake websites", "Unblock all websites"})
+        Me.ComboBox1.Location = New System.Drawing.Point(12, 274)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(256, 21)
+        Me.ComboBox1.TabIndex = 3
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(12, 324)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 4
+        Me.Button3.Text = "Fix"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Location = New System.Drawing.Point(12, 300)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(256, 20)
+        Me.ProgressBar1.TabIndex = 5
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(283, 382)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.Text = "Kissanime Fix"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents Button3 As Button
+    Friend WithEvents ProgressBar1 As ProgressBar
+End Class
---
+++ Kissanime Fakeblocker/Form1.vb
@@ -0,0 +1,34 @@
+﻿Public Class Form1
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Close()
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Dim Hosts As String = Environment.SystemDirectory & "\drivers\etc\hosts"
+        Dim sr As New IO.StreamReader(Hosts)
+        Dim input As String = sr.ReadToEnd()
+        sr.Close()
+        Dim output As String = input + vbNewLine + "104.25.20.21 kiss-anime.me"
+        Dim output1 As String = vbNewLine + "104.25.20.21 kissanime.to"
+        Dim output2 As String = vbNewLine + "104.25.20.21 kissanime.ru"
+        Dim output3 As String = vbNewLine + "104.25.20.21 kissanime.io"
+        Dim output4 As String = vbNewLine + "104.25.20.21 kissanime.com"
+        Dim output5 As String = vbNewLine + "104.25.20.21 www.kiss-anime.me"
+        Dim output6 As String = vbNewLine + "104.25.20.21 www.kissanime.to"
+        Dim output7 As String = vbNewLine + "104.25.20.21 www.kissanime.ru"
+        Dim output8 As String = vbNewLine + "104.25.20.21 www.kissanime.io"
+        Dim output9 As String = vbNewLine + "104.25.20.21 www.kissanime.com"
+        Dim sw As New IO.StreamWriter(Hosts)
+        sw.Write(output)
+        sw.Write(output1)
+        sw.Write(output2)
+        sw.Write(output3)
+        sw.Write(output4)
+        sw.Write(output5)
+        sw.Write(output6)
+        sw.Write(output7)
+        sw.Write(output8)
+        sw.Write(output9)
+        sw.Close()
+    End Sub
+End Class
